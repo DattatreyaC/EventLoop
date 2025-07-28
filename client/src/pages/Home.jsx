e0d1b8d700094a31fc91be2e8f3c9519cf255e56
unknown <dattatreya.chakraborty12@gmail.com>
2025-07-28 17:25:21 +0530

    responsive home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,6 +21,12 @@ const Home = () => {
         let tagSplit = SplitText.create(tagRef.current, { type: "words" });
         let locSplit = SplitText.create(locationRef.current, { type: "chars" });
 
+        let bounceDistance = 30;
+        if (buttonRef.current && arrowRef.current) {
+            const capsule = buttonRef.current;
+            bounceDistance = capsule.offsetHeight * 0.5;
+        }
+
         const timeline = gsap.timeline();
 
         timeline.from(headingRef1.current, {
@@ -100,15 +106,15 @@ const Home = () => {
             duration: 0.2,
             stagger: 0.2,
         });
-        timeline.from(
-            buttonRef.current,
-            {
-                autoAlpha: 0,
-                duration: 1.3,
-                y: -10,
-            },
-            "-=0.7",
-        );
+        // timeline.from(
+        //     buttonRef.current,
+        //     {
+        //         autoAlpha: 0,
+        //         duration: 1.3,
+        //         y: -10,
+        //     },
+        //     "-=0.7",
+        // );
         timeline.from(
             loopRef.current,
             {
@@ -120,7 +126,7 @@ const Home = () => {
 
         gsap.to(arrowRef.current, {
             opacity: 1,
-            y: 30,
+            y: bounceDistance,
             duration: 1.25,
             repeat: -1,
             yoyo: true,
@@ -141,29 +147,29 @@ const Home = () => {
             id="home"
             className="min-h-screen py-20 flex flex-col items-center justify-center bg-black/40"
         >
-            <div className=" p-40 flex flex-col relative items-center justify-center ">
+            <div className="p-30 sm:p-35 md:p-40 lg:p-45 flex flex-col relative items-center justify-center ">
                 <img
                     ref={loopRef}
                     src="loop.gif"
                     alt="loop"
-                    className="absolute w-50 bottom-[-13%] z-10 opacity-50"
+                    className="absolute w-34 sm:w-44 md:w-53 bottom-[-12%] z-10 opacity-50 "
                 />
 
-                <div className=" flex flex-col p-15 py-7 items-center justify-center relative">
+                <div className=" flex flex-col p-15 py-7 items-center justify-center relative ">
                     <h3
                         ref={locationRef}
-                        className="absolute flex top-0 left-0 text-red font-bold text-xl"
+                        className="absolute flex top-0 left-0 text-red font-bold text-sm sm:text-[16px] md:text-lg lg:text-xl"
                     >
                         {/* location icon */}
                         <i
                             ref={pinRef}
-                            className="ri-map-pin-line font-thin text-xl text-white "
+                            className="ri-map-pin-line font-thin text-md sm:text-[16px] md:text-lg lg:text-xl text-white "
                         ></i>
                         JADAVPUR
                         <span className="text-white">&nbsp;UNIVERSITY</span>
                     </h3>
 
-                    <h1 className="text-9xl font-bold flex">
+                    <h1 className="text-5xl sm:text-[5.5rem] md:text-[6.5rem] lg:text-9xl xl:text-[9rem] font-bold flex">
                         <span
                             ref={headingRef1}
                             className="bg-gradient-to-r from-gray-400 to-white text-transparent bg-clip-text"
@@ -178,7 +184,7 @@ const Home = () => {
                         </span>
                         <span
                             ref={headingDateRef}
-                            className=" bg-gradient-to-br from-yellow-200 via-golden to-yellow-800 bg-clip-text text-transparent font-thin text-8xl"
+                            className=" bg-gradient-to-br from-yellow-200 via-golden to-yellow-800 bg-clip-text text-transparent font-thin text-3xl sm:text-5xl md:text-[4rem] lg:text-8xl xl:text-[6.75rem]"
                         >
                             '25
                         </span>
@@ -186,18 +192,18 @@ const Home = () => {
 
                     <h3
                         ref={dateRef}
-                        className="absolute bottom-0 right-0 text-2xl font-thin"
+                        className="absolute bottom-0 right-0 text-sm sm:text-lg md:text-[20px] lg:text-2xl font-thin"
                     >
                         {/* calendar icon */}
                         <i
                             ref={calRef}
-                            className="ri-calendar-event-line font-extralight text-[18px]"
+                            className="ri-calendar-event-line font-extralight text-sm sm-text-[16px] lg:text-[18px]"
                         ></i>{" "}
                         August&nbsp;
-                        <span className="text-golden font-bold animate-pulse text-4xl">
+                        <span className="text-golden font-bold animate-pulse text-lg sm:text-2xl md:text-3xl lg:text-4xl">
                             28
                         </span>
-                        , <span className="text-red font-semibold">2025</span>
+                        , <span className="text-red font-medium">2025</span>
                     </h3>
                 </div>
 
@@ -209,23 +215,42 @@ const Home = () => {
                 <p
                     id="tagline"
                     ref={tagRef}
-                    className="text-2xl font-black italic text-golden"
+                    className=" text-xs sm:text-lg lg:text-2xl font-black italic text-golden"
                 >
                     Welcome to the Ultimate Async Experience !
                 </p>
 
-                <a
+                {/* <a
                     href="#events"
-                    ref={buttonRef}
-                    className="flex border-1 border-gray-400/50 hover:border-white text-center w-max absolute bottom-0 text-lg font-thin rounded-full px-1.5 h-15 bg-gradient-to-br from-gray-800/30 via-gray-400/20  to-white/20  hover:shadow-[0_0_3px_red,0_0_10px_white] transition-shadow duration-200 z-20"
+                    // ref={buttonRef}
+                    className="flex border-1 border-gray-400/50 hover:border-white text-center w-max absolute bottom-0 text-lg font-thin rounded-full px-1 md:px-1.5 h-10 md:h-15 bg-gradient-to-br from-gray-800/30 via-gray-400/20  to-white/20  hover:shadow-[0_0_3px_red,0_0_10px_white] transition-shadow duration-200 z-20"
                 >
                     <div className="relative w-3 h-full">
                         <i
                             id="down-arrow"
                             ref={arrowRef}
-                            className="ri-arrow-down-s-fill text-lg opacity-30 absolute top-0 left-[-25%] "
+                            className="ri-arrow-down-s-fill text-sm md:text-lg opacity-30 absolute top-0 left-[-25%] border"
                         ></i>
                     </div>
+                </a> */}
+
+                <a
+                    href="#events"
+                    ref={buttonRef}
+                    className="absolute bottom-0 sm:bottom-0 md:bottom-0 left-1/2 -translate-x-1/2 transform flex items-start justify-center 
+                            w-5 sm:w-7 md:w-7 
+                            h-12 sm:h-16 md:h-18 
+                            rounded-full 
+                            border border-gray-500/50 hover:border-white 
+                            bg-gradient-to-br from-gray-900/70 via-gray-600 to-gray-300/60 
+                            transition-shadow duration-200 
+                            hover:shadow-[0_0_3px_red,0_0_10px_white] 
+                            z-20 "
+                >
+                    <i
+                        ref={arrowRef}
+                        className="ri-arrow-down-s-fill text-sm sm:text-xl md:text-2xl opacity-70 w-full text-center"
+                    ></i>
                 </a>
             </div>
         </section>
